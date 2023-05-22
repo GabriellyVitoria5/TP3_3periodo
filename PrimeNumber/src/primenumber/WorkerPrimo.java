75d596bf8422c38b980738ea37855bc1fb854635
GabriellyVitoria5 <86209514+GabriellyVitoria5@users.noreply.github.com>
2023-05-22 12:57:52 -0300

    Update WorkerPrimo.java

--- PrimeNumber/src/primenumber/WorkerPrimo.java
+++ PrimeNumber/src/primenumber/WorkerPrimo.java
@@ -14,14 +14,10 @@ public class WorkerPrimo extends Thread {
     private static Object chaveRecurso = new Object();
     private static boolean existeTrabalho = true;
 
-    private StringBuilder palavraSemCharEspecial = new StringBuilder();
-    private String[] palavras;
-    private int quantPrimos;
     private long maiorPrimo = 2;
 
     public WorkerPrimo(ArrayList tarefas) {
         this.tarefas = tarefas;
-        this.quantPrimos = 0;
     }
 
     public long getMaiorPrimo() {
@@ -51,66 +47,24 @@ public class WorkerPrimo extends Thread {
                     //leitura das linhas do arquivo
                     String linha = null;
                     long numero;
-                    
+
                     linha = bufLeitura.readLine();
-                    while (linha != null) {                        
-                        palavras = linha.split("[,:;?!'(){}\\s]"); // Separar palavras com base em espaços em branco e em alguns caracteres especiais
+                    while (linha != null) {
+                        String[] palavras = linha.split("[,:;?!'(){}\\s]"); // Separar palavras com base em espaços em branco e em alguns caracteres especiais
 
                         for (String palavra : palavras) {
                             // Realizar alguma ação com a palavra separada
                             //System.out.println(palavra);
-                            //System.out.println(palavra.replaceAll("[,.:;!?{}'()]", ""));
+                            //System.out.println(palavra.replaceAll("[,:;!?{}'()]-", ""));
 
-                            //não sei se usar StringBuilder é melhor
-                            //palavraSemCharEspecial.append(palavra.replaceAll("[,:;!?{}'()]", ""));
-                            //System.out.println(palavraSemCharEspecial.toString());
-                            //palavraSemCharEspecial.setLength(0);
-                            
-                            
                             numero = enontrarNumero(palavra);
-                            if(numero > maiorPrimo && isPrimo(numero)){
+                            if (numero > maiorPrimo && isPrimo(numero)) {
                                 maiorPrimo = numero;
                             }
-                            
+
                         }
                         linha = bufLeitura.readLine();
                     }
-
-
-                    
-                    /*
-                    tava dando erro de NullPointerException na linha 85 
-                    acho que é porque todo final de arquivo e linha é nula
-                    enfim, aí só troquei o do while pelo while aí de cima
-                    pode deixar assim mesmo?
-                    */
-                    /*do {
-                        linha = bufLeitura.readLine();
-                        //System.out.println(linha);
-
-                        //tem erro aqui de NullPointerException
-                        palavras = linha.split("[,:;?!'(){}\\s]"); // Separar palavras com base em espaços em branco e em alguns caracteres especiais
-
-                        for (String palavra : palavras) {
-                            // Realizar alguma ação com a palavra separada
-                            //System.out.println(palavra);
-                            //System.out.println(palavra.replaceAll("[,.:;!?{}'()]", ""));
-
-                            //não sei se usar StringBuilder é melhor
-                            //palavraSemCharEspecial.append(palavra.replaceAll("[,.:;!?{}'()]", ""));
-                            //System.out.println(palavraSemCharEspecial.toString());
-                            //palavraSemCharEspecial.setLength(0);
-                            
-                            
-                            numero = pegarNumero(palavra);
-                            if(numero > maiorPrimo && isPrimo(numero)){
-                                maiorPrimo = numero;
-                            }
-                            
-                        }
-
-                    } while (linha != null);
-                    */
 
                 } catch (FileNotFoundException ex) {
                     System.err.println("Arquivo não existe no dir.");
@@ -125,7 +79,7 @@ public class WorkerPrimo extends Thread {
             }
         }
     }
-    
+
     //encontrar se a palavra encontrada em uma linha é um número
     private long enontrarNumero(String str) {
         long numero;
@@ -139,7 +93,15 @@ public class WorkerPrimo extends Thread {
 
     //achar um jeito melhor de encontrar o primo, não é pra usar esse
     private boolean isPrimo(long numero) {
-        int quantDivExatas = 0;
+        for (long i = 2; i <= numero / 2; i++) {
+
+            if ((numero % i) == 0) {
+                return false; //Assim que encontra um divisor, sabe que o número não é primo.
+            }
+        }
+        return true; //não encontrou divisores, o número é primo
+
+        /*int quantDivExatas = 0;
 
         for (int div = 2; div <= numero - 1; div++) {
             if (numero % div == 0) {
@@ -148,7 +110,7 @@ public class WorkerPrimo extends Thread {
         }
 
         return quantDivExatas == 0;
-
+         */
     }
 
     public void aguarde() {
@@ -174,3 +136,4 @@ public class WorkerPrimo extends Thread {
     }
 
 }
+
