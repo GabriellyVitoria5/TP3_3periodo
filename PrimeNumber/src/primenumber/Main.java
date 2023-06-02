00a29733e5f7c00a3060ccae1e0457c5630c5da2
GabriellyVitoria5 <86209514+GabriellyVitoria5@users.noreply.github.com>
2023-06-02 12:15:35 -0300

    Update Main.java

--- PrimeNumber/src/primenumber/Main.java
+++ PrimeNumber/src/primenumber/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static File selecionaDiretorioRaiz() {
         JFileChooser janelaSelecao = new JFileChooser(".");
         //janelaSelecao.setControlButtonsAreShown(false);
-        janelaSelecao.setControlButtonsAreShown(true);
+        janelaSelecao.setControlButtonsAreShown(true); //atributo foi alterado para true para ser possível selecionar uma pasta
 
         //conf. do filtro de selecao
         janelaSelecao.setFileFilter(new FileFilter() {
@@ -51,53 +51,63 @@ public class Main {
         } else {
             //...Modifique a partir daqui
             //AQUI você deve explorar a pasta, arquivos e subpastas...
-            
+
+            //guardar o tempo de quando a execução, depois de escolher o diretório, começou
             long tempoI = System.currentTimeMillis();
-            
-            //calcula a quantidade de "trabalhadores", de threads, com base no número de processadores siponíveis
+
+            //calcula a quantidade de threads ("trabalhadores") com base no número de processadores disponíveis
             WorkerPrimo threads[] = new WorkerPrimo[Runtime.getRuntime().availableProcessors()];
 
-            //array para  armazenar todos 
+            //array para adicionar o diretório principal no topo da pilha
             ArrayDeque<File> explorar = new ArrayDeque<>();
             explorar.push(pastaInicial);
-            
-            //array para armazenar todos os arquivos de texto a serem analizados 
+
+            //array para armazenar todos os arquivos de texto encontrados dentro do diretório principal 
             ArrayList<File> tarefas = new ArrayList<>();
 
             //processo de busca pelo arquivo
             while (!explorar.isEmpty()) {
 
-                //desemp. o diretorio do topo
+                //desempilha o diretorio do topo
                 File diretorioAtual = explorar.pop();
 
+                //array que contém os arquivos e diretórios dentro do diretório atual
                 File arquivosDir[] = diretorioAtual.listFiles();
 
-                //passando por todos os arq. e subDir.
+                //passando por todos os arquivos e subDiretórios
                 for (File arq : arquivosDir) {
-
-                    //verificamos se é uma "pasta", caso afirmativo empilha
+                    /*
+                    verificar se é uma "pasta", caso afirmativo empilha 
+                    caso contrário é preciso verificar se é um arquivo de texto
+                    */
                     if (arq.isDirectory()) {
                         explorar.push(arq);
-                    } else {  
+                    } 
+                    else {
                         if (arq.getAbsolutePath().endsWith(".txt")) {
+                            //arquivo de texto adicionado no vetor de tarefas
                             tarefas.add(arq);
                         }
-
                     }
                 }
 
             }
-            
-            //insta. e executa as threads
-            for(int i = 0; i < threads.length;i++){
+
+            //instancia e executa as threads
+            for (int i = 0; i < threads.length; i++) {
                 threads[i] = new WorkerPrimo(tarefas);
                 threads[i].start();
             }
-            
-            WorkerPrimo.acordaThreads();
+
+            /*
+            Do jeito que foi implementado, todas as threads já recebem o trabalho todo de uma vez,
+            que é analisar os arquivos txt do array de tarefas.
+            Então é preciso sinalizar que não há mais trabalho a ser adicionado a partir daqui
+            */
             WorkerPrimo.termina();
-            
-            for(WorkerPrimo w : threads){
+
+            //espetar todas as threads terminarem seu trabalho
+            for (WorkerPrimo w : threads) {
                 try {
                     w.join();
                 } catch (InterruptedException ex) {
@@ -106,28 +116,34 @@ public class Main {
             }
 
             /*
-            todas as threads terminaram de executar e cada uma achou o maior número primo
-            agora é preciso verificar qual desse números é o maior de todos
-            */            
-            long numPrimo = 2;
-            for (int i = 0; i < threads.length; i++) {
-                if(threads[i].getMaiorPrimo() > numPrimo){
-                    numPrimo = threads[i].getMaiorPrimo();
+            todas as threads terminaram de executar e cada uma achou o seu maior número primo
+            agora é preciso verificar qual deles é o maior de todos
+            */
+            long numPrimo = 2; //o menor primo possível é 2
+            String arquivoMaiorPrimo = null; //enccontrar qual o arquivo em que foi encontrado o maior primo
+            for (WorkerPrimo thread : threads) {
+                //System.out.println(thread.getMaiorPrimo());
+                if (thread.getMaiorPrimo() > numPrimo) {
+                    numPrimo = thread.getMaiorPrimo();
+                    arquivoMaiorPrimo = thread.getArquivoMaiorPrimo();
                 }
             }
-            
+
+            //guardar o tempo em que o programa encontrou o maior pimo
             long tempoF = System.currentTimeMillis();
-            // exibe o tempo de execução
             
-            JOptionPane.showMessageDialog(null, "O maior número primo é:" + numPrimo,
+            //exibe o resultado encontrado
+            JOptionPane.showMessageDialog(null, "O maior número primo é:" + numPrimo, 
                     "resultado", JOptionPane.INFORMATION_MESSAGE);
-            
-            JOptionPane.showMessageDialog(null, ("Tempo de execução: " + 
-                    ((tempoF - tempoI) / 3600000) + "h "
+            JOptionPane.showMessageDialog(null, "Encontrado no arquivo: " + arquivoMaiorPrimo, 
+                    "resultado", JOptionPane.INFORMATION_MESSAGE);
+
+            //exibe o tempo de execução
+            JOptionPane.showMessageDialog(null, ("Tempo de execução: "
+                    + ((tempoF - tempoI) / 3600000) + "h "
                     + ((tempoF - tempoI) % 3600000 / 60000) + "min "
                     + ((tempoF - tempoI) % 60000 / 1000) + "seg "
                     + ((tempoF - tempoI) % 1000) + "ms"));
         }
     }
 }
-
